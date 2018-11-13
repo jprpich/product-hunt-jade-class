2e4eb539636a2979a202ef8a00a08ad201e21bb8
Joshua <joshuaprpich@gmail.com>
2018-11-13 10:44:58 -0500

    implement authenticaction

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
   root 'products#index'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+
+  resources :users, only: [:new, :create]
   resources :products
 
 end
